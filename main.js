91cf0c0a0c2601d33a6ab93ce0add089a7cf11d2
Jose Alcaraz <jose.david.alcaraz7@gmail.com>
2022-12-28 12:00:59 -0500

    Collapse feature and UI improvements

--- main.js
+++ main.js
@@ -4,14 +4,24 @@ function init() {
 
 function events() {
     var btnChangeMode = document.getElementById("btn-change-mode");
+    var currentIdNum = 0;
+    // var btnChangeMode
 
     // document.addEventListener("scroll", (event) => {
     //     sectionDots.classList.add("--animate-fadein");
     // });
+    
+    // portfolio_item--active
 
+    document.querySelectorAll('.portfolioItem').forEach(item => {
+        item.addEventListener('click', event => {
+            currentIdNum = parseInt(event.target.id.replace( /^\D+/g, ''));
+            event.target.classList.toggle("portfolio_item--active");
+        })
+    }, false);
+    
     btnChangeMode.addEventListener('click', event => {
-        console.log (event.target.id);
-        document.body.classList.toggle("--light-mode");
+        document.body.classList.toggle("--dark-mode");
     }, false);
 }
 
