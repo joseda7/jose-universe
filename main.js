cfd7f7381aac06d0b819a38db44b40e0260493f5
Jose Alcaraz <jose.david.alcaraz7@gmail.com>
2023-01-03 23:47:47 -0500

    Adding new content and some responsive UI

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const CONSOLE_MSG = [
     "I've had a great time creating and building cool projects with amazing people. Check out some of them",
-    "Both illustration animation have been there in my hearth for a while",
+    "Both illustration and animation have been around since I was a kid",
     "If you think we can collaborate together feel free to send me a message. It will be a massive honor to hear from you"
 ]
 
@@ -10,7 +10,6 @@ function init() {
 
 function events() {
     var btnChangeMode = document.getElementById("btn-change-mode");
-    var btnContact = document.getElementById("btn-contact");
     var consoleTxtElement1 = document.getElementById("console-text-1");
     var consoleTxtElement2 = document.getElementById("console-text-2");
     var consoleTxtElement3 = document.getElementById("console-text-3");
@@ -24,17 +23,13 @@ function events() {
             consoleTxtElement2.innerHTML = CONSOLE_MSG[1] + "<span>_</span>";
             consoleTxtElement2.classList.add("--animate-typing");{}
         }
-        if (window.scrollY < 1200) {
-            btnContact.classList.remove("--hide");
-        } 
         if (window.scrollY > 1200) {
             consoleTxtElement3.innerHTML = CONSOLE_MSG[2] + "<span>_</span>";
             consoleTxtElement3.classList.add("--animate-typing");
-            btnContact.classList.add("--hide");
         } 
     }, false);
 
-    document.querySelectorAll('.portfolioItem').forEach(item => {
+    document.querySelectorAll('.portfolio_item').forEach(item => {
         item.addEventListener('click', event => {
             event.target.classList.toggle("portfolio_item--active");
         })
